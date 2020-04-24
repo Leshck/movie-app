113f35fbedf408509f2106c439f7472ea4d7378a
Aliaksei <aliaksei.autushonak@gmail.com>
2020-04-24 19:11:05 +0300

    feat: [AA] task 1, multiple components

---
+++ src/app/index.js
@@ -0,0 +1,4 @@
+export { default as HelloWorldComponent } from './component';
+export { default as HelloWorldPureComponent } from './pure-component';
+export { default as HelloWorldFuncComponent } from './func-component';
+export { default as HelloWorldCreateElement } from './create-element';
